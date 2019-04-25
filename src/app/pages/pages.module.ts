40de2fa285b04c879d29de46106b88f740b055d7
Josue Gabuardi <josuegabuardi@gmail.com>
2019-04-25 02:45:53 -0600

    create new theme-test component

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,9 +2,10 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {LandingComponent} from './landing/landing.component';
 import {MaterialModule} from '../libraries-modules/material/material.module';
+import { ThemeTestComponent } from './theme-test/theme-test.component';
 
 @NgModule({
-  declarations: [LandingComponent],
+  declarations: [LandingComponent, ThemeTestComponent],
   imports: [
     CommonModule,
     MaterialModule
---
+++ src/app/pages/theme-test/theme-test.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-theme-test',
+  templateUrl: './theme-test.component.html',
+  styleUrls: ['./theme-test.component.sass']
+})
+export class ThemeTestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
