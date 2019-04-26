20827f7a41fcf90670dade4514e7c41dc5253912
Josue Gabuardi <josuegabuardi@gmail.com>
2019-04-26 00:39:14 -0600

    add landing title

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,12 +3,14 @@ import {CommonModule} from '@angular/common';
 import {LandingComponent} from './landing/landing.component';
 import {MaterialModule} from '../libraries-modules/material/material.module';
 import { ThemeTestComponent } from './theme-test/theme-test.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
   declarations: [LandingComponent, ThemeTestComponent],
   imports: [
     CommonModule,
-    MaterialModule
+    MaterialModule,
+    FlexLayoutModule
   ],
   exports: [LandingComponent]
 })
