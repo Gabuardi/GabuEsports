ba2766875094352a14acaee36bd1b77b901a2c53
Josue Gabuardi <josuegabuardi@gmail.com>
2019-05-05 13:55:11 -0600

    rezise format image

--- src/app/modules/font-awesome/angular-font-awesome.module.ts
+++ src/app/modules/font-awesome/angular-font-awesome.module.ts
@@ -3,9 +3,9 @@ import { CommonModule } from '@angular/common';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import {faMedium, faDiscord, faGithub, faTwitter} from '@fortawesome/free-brands-svg-icons';
-import {faSignInAlt, faSignOutAlt, faTrophy, faHatWizard} from '@fortawesome/free-solid-svg-icons';
+import {faSignInAlt, faSignOutAlt, faTrophy, faHatWizard, faCalendarAlt} from '@fortawesome/free-solid-svg-icons';
 
-library.add(faMedium, faDiscord, faGithub, faTwitter, faSignInAlt, faSignOutAlt, faTrophy, faHatWizard);
+library.add(faMedium, faDiscord, faGithub, faTwitter, faSignInAlt, faSignOutAlt, faTrophy, faHatWizard, faCalendarAlt);
 
 @NgModule({
   declarations: [],
