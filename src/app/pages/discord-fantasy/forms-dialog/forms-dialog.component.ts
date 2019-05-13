f25d81bce8792d64d09f66b68ae867729d4e9344
Josue Gabuardi <josuegabuardi@gmail.com>
2019-05-13 13:40:00 -0600

    update data

--- src/app/pages/discord-fantasy/forms-dialog/forms-dialog.component.ts
+++ src/app/pages/discord-fantasy/forms-dialog/forms-dialog.component.ts
@@ -24,6 +24,7 @@ export class FormsDialogComponent {
         {name: 'Día 2', url: 'https://forms.gle/6cM49T7QTGv7qHHF7'},
         {name: 'Día 3', url: 'https://forms.gle/Jbc9pjxixU8BK6DW7'},
         {name: 'Día 4', url: 'https://forms.gle/qZ3nhVkxhf7pzE7u5'},
+        {name: 'Día 5', url: 'https://forms.gle/np9mJpEmW5wDH8n57'}
       ]
     };
   }
